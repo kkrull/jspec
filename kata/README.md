3035cad224542d9a4c57e76d721860e3dddd06fa
Kyle Krull <kkrull@8thlight.com>
2020-03-03 09:10:02 -0600

    Add Makefile

---
+++ kata/README.md
@@ -0,0 +1,10 @@
+# Kata with JavaSpec 2.x
+
+This is built with a separate process, to ensure separation between the Kata's codebase and
+JavaSpec's.
+
+```
+$ make clean
+$ make
+$ make run
+```
--- kata/build-and-run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-rm -Rf out
-mkdir -p out/production/kata
-javac -d out/production/kata -cp ../lambdas-and-strings/lambda-api/build/libs/lambda-api-2.0.0-SNAPSHOT.jar:/Users/kkrull/.m2/repository/org/hamcrest/hamcrest/2.2/hamcrest-2.2.jar src/FizzBuzzSpecs.java 
-javaspec run --reporter=plaintext --spec-classpath=/Users/kkrull/.m2/repository/org/hamcrest/hamcrest/2.2/hamcrest-2.2.jar:out/production/kata FizzBuzzSpecs
-
