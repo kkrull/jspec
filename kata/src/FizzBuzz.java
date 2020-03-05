c1995fd7fdb793d73cbc5c6723cd5cda7d7aa74a
Kyle Krull <kkrull@8thlight.com>
2020-03-04 18:08:35 -0600

    Extract FizzBuzz class

---
+++ kata/src/FizzBuzz.java
@@ -0,0 +1,8 @@
+public class FizzBuzz {
+  public static String encode(int number) {
+    if(number == 3)
+      return "fizz";
+
+    return String.valueOf(number);
+  }
+}
--- kata/src/FizzBuzzSpecs.java
+++ kata/src/FizzBuzzSpecs.java
@@ -15,13 +15,4 @@ public class FizzBuzzSpecs {
       });
     });
   }
-
-  public static class FizzBuzz {
-    public static String encode(int number) {
-      if(number == 3)
-        return "fizz";
-      
-      return String.valueOf(number);
-    }
-  }
 }
